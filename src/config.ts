3e2b7307b2afea410d438ae8c37dfdf2f8b3ad0c
Lyubansky <dlyubansky@gmail.com>
2025-12-25 09:24:18 +0200

    .

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const defaultConfig: WidgetConfig = {
   source: 'taboola',
   publisherId: 'taboola-templates',
   appType: 'desktop',
-  apiKey: 'f9040ab1b9c802857aa783c469d0e0ff7e7366e4',
+  apiKey: 'REPLACE_WITH_YOUR_API_KEY',
   sourceId: 'demo-source-id',
   sourceType: 'video',  
   sourceUrl: window.location.href,
